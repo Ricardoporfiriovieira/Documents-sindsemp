0377e5a09010dd5cc91601b65cd148735d3cd956
Ricardo Porfírio <89818885+Ricardoporfiriovieira@users.noreply.github.com>
2022-09-05 13:59:43 -0300

    Delete Sindsemp-DOCUMENTOS directory

--- Sindsemp-DOCUMENTOS/Documents.js
+++
@@ -1,8 +0,0 @@
-// Mobile hamburger menu
-const hamburgerMenu = document.querySelector(".hamburger");
-const hamburgerMenuDropdown = document.querySelector(".dropdown");
-
-hamburgerMenu.addEventListener("click", function () {
-  this.classList.toggle("is-active");
-  hamburgerMenuDropdown.classList.toggle("is-active");
-});
