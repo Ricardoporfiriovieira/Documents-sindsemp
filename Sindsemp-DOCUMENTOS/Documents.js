0378c8775ac0a087314862dc77d12e62c5f00ad2
Ricardo Porfírio <89818885+Ricardoporfiriovieira@users.noreply.github.com>
2022-09-05 14:01:17 -0300

    Add files via upload

---
+++ Sindsemp-DOCUMENTOS/Documents.js
@@ -0,0 +1,8 @@
+// Mobile hamburger menu
+const hamburgerMenu = document.querySelector(".hamburger");
+const hamburgerMenuDropdown = document.querySelector(".dropdown");
+
+hamburgerMenu.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  hamburgerMenuDropdown.classList.toggle("is-active");
+});
