573c0956e28f70c9f6a46b6382b49478b700c2d6
Ricardo Porfírio Vieira <ricardoporfiriovieira@gmail.com>
2022-09-23 19:44:42 -0300

    adicionando alterações no header

--- Sindsemp-DOCUMENTOS/Documents.js
+++ Sindsemp-DOCUMENTOS/Documents.js
@@ -1,8 +1,20 @@
 // Mobile hamburger menu
 const hamburgerMenu = document.querySelector(".hamburger");
 const hamburgerMenuDropdown = document.querySelector(".dropdown");
+const submenu = document.querySelector(".dropdown-sub");
+const btnSubmenu = document.querySelector(".btn-submenu");
 
 hamburgerMenu.addEventListener("click", function () {
   this.classList.toggle("is-active");
   hamburgerMenuDropdown.classList.toggle("is-active");
 });
+
+
+
+
+
+
+btnSubmenu.addEventListener("click", function(){
+  hamburgerMenu.classList.toggle("is-active");
+  submenu.classList.toggle("is-active");
+})
--- Sindsemp-HOME/Home.js
+++ Sindsemp-HOME/Home.js
@@ -2,6 +2,7 @@
 const hamburgerMenu = document.querySelector(".hamburger");
 const hamburgerMenuDropdown = document.querySelector(".dropdown");
 
+
 hamburgerMenu.addEventListener("click", function () {
   this.classList.toggle("is-active");
   hamburgerMenuDropdown.classList.toggle("is-active");
